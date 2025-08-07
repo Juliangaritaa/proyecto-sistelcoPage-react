9e94862a48d12205448c3075199e36d1029eda68
Julian Felipe Angarita Alarcon <julian.angarita11@soy.sena.edu.co>
2025-08-07 14:56:19 -0500

    change page description

--- proyecto-sistelco-react/src/data/dataSISTELCO.ts
+++ proyecto-sistelco-react/src/data/dataSISTELCO.ts
@@ -14,7 +14,7 @@ export const dataSISTELCOInicio = {
     inicio: {
         title: "Bienvenido a SISTEL Telecomunicaciones",
         subtitle: "Soluciones integrales en telecomunicaciones",
-        description: "En SISTEL, ofrecemos servicios de instalación, mantenimiento y soporte técnico para garantizar la mejor experiencia en telecomunicaciones.",
+        description: "En SISTEL Telecomunicaciones, ofrecemos servicios de conexión a Internet banda ancha y de canal dedicado; mediante la instalación de equipos inalámbricos  de radio enlace, microondas  y  fibra óptica de última tecnología. Brindando  un soporte técnico inmediato y un mantenimiento óptimo para garantizar la mejor conectividad y disfrutar de una experiencia sin límites en telecomunicaciones. Tenemos planes para tu hogar o empresa y de acuerdo a tu presupuesto. Contáctanos  y consulta por nuestras promociones.",
         image: "/images/inicio/2148456294.jpg"
     }
 };
@@ -23,7 +23,7 @@ export const dataSISTELCOServicios = {
     servicios: {
         title: "Nuestros Servicios",
         subtitle: "Internet Fibra Óptica",
-        description: "En SISTEL Telecomunicaciones, ofrecemos servicios de conexión a Internet banda ancha y de canal dedicado; mediante la instalación de equipos inalámbricos  de radio enlace, microondas  y  fibra óptica de última tecnología. Brindando  un soporte técnico inmediato y un mantenimiento óptimo para garantizar la mejor conectividad y disfrutar de una experiencia sin límites en telecomunicaciones. Tenemos planes para tu hogar o empresa y de acuerdo a tu presupuesto. Contáctanos  y consulta por nuestras promociones.",
+        description: "En SISTEL, ofrecemos una amplia gama de servicios para satisfacer todas tus necesidades en telecomunicaciones.",
         servicesList: [
             { name: "Descargas Ilimitadas", icon: "Download" },
             { name: "Velocidad Simétrica", icon: "Activity" },
